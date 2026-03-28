6f9f63ac9e2db0bf9f887da7a9f75de3ff7f6e3c
Tasiu Ismail <mynameistasiu@gmail.com>
2026-03-28 06:38:11 +0100

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layout';
 import { saveTx } from '../utils/storage';
 
 const CODE_PRICE = 7000;
-const WA = '+2348136347797';
+const WA = '+2348161662371';
 
 export default function Checkout() {
   const router = useRouter();
