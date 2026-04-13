e46b707072316da4adc088d55c74aa91965bcb59
Tasiu Ismail <mynameistasiu@gmail.com>
2026-04-13 20:16:46 +0100

    Update buy-code.js

--- pages/buy-code.js
+++ pages/buy-code.js
@@ -5,7 +5,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import Layout from "../components/Layout";
 
 const CODE_PRICE = 7000;
-const WA = "+2347085462173";
+const WA = "+2348136027182";
 
 function PurchaseLogo({ size = 34 }) {
   return (
